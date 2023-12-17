9aebc634473b9258ca1b0a5d17781b66c55d3e6e
NuTipoMaksim <150586783+NuTipoMaksim@users.noreply.github.com>
2023-12-17 13:16:37 +0300

    Update bot_logic.py

--- bot_logic.py
+++ bot_logic.py
@@ -1,11 +1,22 @@
-import random
+import discord
+from discord.ext import commands
+import os, random
+print(os.listdir('images'))
+intents = discord.Intents.default()
+intents.message_content = True
 
+bot = commands.Bot(command_prefix = '$', intents = intents)
 
-def gen_pass(pass_length):
-    elements = "+-/*!&$#?=@<>"
-    password = ""
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
 
-    for i in range(pass_length):
-        password += random.choice(elements)
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'а ты знал, что пластиковые бутылки разлагаются от 400 до 700 лет! а пакеты от 100 до 200 лет! а вот последствия:')
+    img_name = random.choice(os.listdir('image'))
+    with open(f'image/{img_name}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
 
-    return password
+bot.run("MTE3ODI3NDA2NTc4MjQxOTQ2Ng.GJogk6.xFJubGiyG2ewfaiqx1zF8witAUAFk1NDBSm4Uo")
